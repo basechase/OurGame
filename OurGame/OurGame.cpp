a99eefb5618d6d4cdbe4b5c1c7767a6c5e60d5e5
basechase <chase7y@gmail.com>
2025-01-12 14:07:40 -0600

    added mathlibrary

--- OurGame/OurGame.cpp
+++ OurGame/OurGame.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <raylib.h>
+#include "Matrix4.h"
+#include "Matrix3.h"
+#include "Vector3.h"
+#include "Vector4.h"
 
 Color green = { 173, 204, 96, 255 };
 Color darkGreen = { 43, 51, 24, 255 };
@@ -19,7 +23,7 @@ int main()
 	InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake Game");
 	SetTargetFPS(60);
 
-	while (WindowShouldClose() == false)
+	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 
